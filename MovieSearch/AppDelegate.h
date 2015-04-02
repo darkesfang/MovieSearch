7a823729924bc6de4047be3708a6142ffe960f67
Darkes <darkesfang@gmail.com>
2015-04-02 18:57:28 +0800

    Initial Commit

---
+++ MovieSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieSearch
+//
+//  Created by Darkes on 2015/4/2.
+//  Copyright (c) 2015年 Boitec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieSearch
+//
+//  Created by Darkes on 2015/4/2.
+//  Copyright (c) 2015年 Boitec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
